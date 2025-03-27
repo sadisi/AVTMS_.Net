4282d7450c11d69779fc9c1c7c1075a2af72ad6c
sadisi <sadisi.rajapaksha218346@gmail.com>
2025-03-28 00:02:16 +0530

    added login page models and requirements

--- AVTMS/ViewModels/ChangePasswordViewModel.cs
+++ AVTMS/ViewModels/ChangePasswordViewModel.cs
@@ -2,12 +2,8 @@
 
 namespace AVTMS.ViewModels
 {
-    public class RegisterViewModel
+    public class ChangePasswordViewModel
     {
-
-        [Required(ErrorMessage ="Name is required.")]
-        public string Name { get; set; }
-
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string Email { get; set; }
@@ -15,12 +11,14 @@ namespace AVTMS.ViewModels
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
         [DataType(DataType.Password)]
-        [Compare("ConfirmPassword", ErrorMessage = "Password does not match." )]
-        public string Password { get; set; }
+        [Display(Name = "New Password")]
+        [Compare("ConfirmNewPassword", ErrorMessage = "Password does not match.")]
+        public string NewPassword { get; set; }
 
         [Required(ErrorMessage = "ConfirmPassword is required.")]
         [DataType(DataType.Password)]
-        public string ConfirmPassword { get; set; }
-       
-    }
+        [Display(Name = "Confirm New Password")]
+        public string ConfirmNewPassword { get; set; }
+        
+        }
 }
